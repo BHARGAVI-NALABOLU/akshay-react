ef67ebe6b34961c00daa5666ba7ec5753e82cde3
bhargavireddy004 <bhargavi1004nalubolu@gmail.com>
2023-10-03 22:56:12 +0530

    Laying the Foundation.

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
-{
-  /* <div id="Parent">
-  <div id="child1">
-    <h1>I'm an h1 tag</h1>
-    <h2>I'm an h1 tag</h2>
-  </div>
-  <div id="child2">
-    <h1>I'm an h1 tag</h1>
-    <h2>I'm an h1 tag</h2>
-  </div>
-</div>; */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
-console.log(parent); //object
+// React.createElement=>ReactElement-JS Object => HTMLElement(render)
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+// console.log("heading", heading);
+
+//JSX=> Babel transpiler to it React.createElement=>ReactElement-Js Object => HTMLElement(render)
+
+//React Element
+
+//React Functional Component
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+    {/* <HeadingComponent />
+    <HeadingComponent></HeadingComponent>
+    {HeadingComponent()} */}
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # akshay-react
+
+# Parcel
+
+-Dev Build
+-Local Server
+-HMR - Hot Module Replacement
+-File Matching Algorithm -written in C++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistant Hashing
+-Code Splitting
+-Differential Bundling - support older browsers
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bundles
